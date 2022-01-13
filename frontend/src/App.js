da47235d72a8c4eecff6ab323294b415d3038eaa
michaelc42 <michaelcasey42@gmail.com>
2022-01-13 15:16:50 -0600

    Stable before testing REST routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+    </main>
+    <Footer />
+    </Router>
       
-    </div>
-  );
+  )  
 }
 
 export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+export const Footer = () => {
+    return (
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>
+                    Copyright &copy; ProShop
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Footer;
